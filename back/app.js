f9a8c80102dece5aa5eaf319c29c5200fc95cf0f
Anis <mrmezianea@gmail.com>
2022-02-26 15:28:31 +0100

    add sauce

--- back/app.js
+++ back/app.js
@@ -5,6 +5,8 @@ const userRoutes = require('./routes/user');
 
 const Sauce = require('./models/Sauce');
 
+const sauceRoutes = require('./routes/sauce');
+
 mongoose.connect('mongodb+srv://Rengodz:tFJTScQwZvpIQkMl@cluster0.s2oop.mongodb.net/Cluster0?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -23,26 +25,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-    const sauce = new Sauce({
-        ...req.body
-    });
-    sauce.save()
-        .then(() => res.status(201).json({ message: 'objet saved' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({ error }));
-});
-
-app.use('/api/stuff', (req, res, next) => {
-    Sauce.find()
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/sauce', sauceRoutes);
 
 app.use('/api/auth', userRoutes);
 
--- back/models/Sauce.js
+++ back/models/Sauce.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
     title: { type: String, required: true },
+    manufacturer: { type: String, required: true },
     description: { type: String, required: true },
     imageUrl: { type: String, required: true },
+    mainPepper: { type: String, required: true },
     userId: { type: String, required: true },
+    heat: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
